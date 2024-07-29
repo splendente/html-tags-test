44292fc6ff0db78c3cc426760472f001dc1ff1fa
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-07-29 22:13:13 +0900

    :sparkles: create zod schema and HTML tag list

---
+++ src/libs/zod/index.ts
@@ -0,0 +1,12 @@
+import * as z from 'zod'
+import { htmlTagList } from '@/constants/html-tag-list'
+
+export const schema = z.object({
+  element: z
+    .string({
+      invalid_type_error: 'Must be a string'
+    })
+    .refine((element) => htmlTagList.includes(element), {
+      message: 'Invalid HTML element'
+    }),
+})
